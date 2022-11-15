594e3c4c5f3771b60c1ca24c8bb65b1323f34193
Kalid Ruiz <118288206+KrC234@users.noreply.github.com>
2022-11-15 10:28:58 -0600

    Add files via upload

---
+++ src/class/Generador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Generador {
+
+    private int ns; //Numero de salas//
+    private int [][] mapa; //arreglo de mapa dependiente a la variable ns//
+
+
+
+    public int DimeSalas(){
+        //Clase para especificar la cantidad de salas//
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Especifica la cantidad de salas a generar");
+        ns = sc.nextInt();
+        System.out.println("La cantidad de salas a generar es"+ns+"x"+ns);
+        return ns;
+    }
+
+
+
+
+    public void PintaSalas(){
+        //Clase dedicada para generar las salas//
+        mapa = new int [ns][ns];
+        
+    }
+}
+
+
